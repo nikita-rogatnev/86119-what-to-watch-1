b65a06938990fe71e0148f98605812e4cbf02a6e
Rogatnev Nikita <nikita.rogatnev@omnigon.com>
2019-05-22 16:34:18 +0300

    reducer init, tests update

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -5,18 +5,18 @@ import moviesList from './mocks/movies';
 describe(`Action creators work correctly`, () => {
   const ALL_GENRES = `All genres`;
 
-  it(`Action creator for genre change returns correct action`, () => {
+  it(`Genre change returns correct action`, () => {
     expect(ActionCreator.changeGenre(`comedies`)).toEqual({
       type: `CHANGE_GENRE`,
       payload: `comedies`,
     });
   });
 
-  it(`Action creator for genre based movie filtering returns correct movie array when genre is chosen`, () => {
+  it(`Filtering returns correct movie array`, () => {
     expect(ActionCreator.getMovies(`comedies`, moviesList));
   });
 
-  it(`Action creator for genre based movie filtering returns array of all movies when genre is defaulted to all genres`, () => {
+  it(`Filtering returns array of all movies when genre is defaulted`, () => {
     expect(ActionCreator.getMovies(ALL_GENRES, moviesList));
   });
 });
