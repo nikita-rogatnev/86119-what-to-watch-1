6a9f7eb3d185a040d93fea6cd6272f66773fc63f
Rogatnev Nikita <nikita.rogatnev@omnigon.com>
2019-05-23 15:52:40 +0300

    fixes after mentors review

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,17 +4,17 @@ import moviesList from './mocks/movies';
 
 describe(`Action creators work correctly`, () => {
   it(`Genre change returns correct action`, () => {
-    expect(ActionCreator.changeGenre(`comedies`)).toEqual({
-      type: `CHANGE_GENRE`,
+    expect(ActionCreator.filterMovies(`comedies`)).toEqual({
+      type: `FILTER_MOVIES`,
       payload: `comedies`,
     });
   });
 
   it(`Filtering returns correct movie array`, () => {
-    expect(ActionCreator.getMovies(`comedies`, moviesList));
+    expect(ActionCreator.filterMovies(`comedies`, moviesList));
   });
 
   it(`Filtering returns array of all movies when genre is defaulted`, () => {
-    expect(ActionCreator.getMovies(`All genres`, moviesList));
+    expect(ActionCreator.filterMovies(`All genres`, moviesList));
   });
 });
