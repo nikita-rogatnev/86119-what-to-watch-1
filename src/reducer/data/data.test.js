f60971a271a19cded04a77fee14810729ba85251
Rogatnev Nikita <nikita.rogatnev@ix.co>
2019-06-25 15:36:55 +0300

    fix favorites logic

--- src/reducer/data/data.test.js
+++ src/reducer/data/data.test.js
@@ -1,6 +1,6 @@
 import MockAdapter from "axios-mock-adapter";
 import {createAPI} from "../../api";
-import {reducer, ActionType, Operations} from "./data";
+import {reducer, ActionType, Operations, ActionCreators} from "./data";
 
 describe(`Data reducer`, () => {
   it(`Should return initial state by default`, () => {
@@ -14,7 +14,7 @@ describe(`Data reducer`, () => {
     });
   });
 
-  it(`Should change filter`, () => {
+  it(`Should changeCurrentFilter`, () => {
     expect(reducer({
       currentFilter: `All genres`,
     }, {
@@ -33,6 +33,26 @@ describe(`Data reducer`, () => {
 
     apiMock
       .onGet(`/films`)
+      .reply(200, [{fake: true}]);
+
+    return dataLoader(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_DATA,
+          payload: [{fake: true}],
+        });
+      });
+  });
+
+  it(`Should make a correct API GET call for promo data`, () => {
+    const dispatch = jest.fn();
+    const api = createAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const dataLoader = Operations.loadData();
+
+    apiMock
+      .onGet(`/films/promo`)
       .reply(200, [{fake: true}]);
 
     return dataLoader(dispatch, jest.fn(), api)
