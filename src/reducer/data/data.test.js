beeee1d2efbbe0e180c3e541dc5fcbb5b5e6c133
Rogatnev Nikita <nikita.rogatnev@ix.co>
2019-06-25 16:04:40 +0300

    add reducers missing tests

--- src/reducer/data/data.test.js
+++ src/reducer/data/data.test.js
@@ -1,6 +1,6 @@
 import MockAdapter from "axios-mock-adapter";
 import {createAPI} from "../../api";
-import {reducer, ActionType, Operations, ActionCreators} from "./data";
+import {reducer, ActionType, ActionCreators, Operations} from "./data";
 
 describe(`Data reducer`, () => {
   it(`Should return initial state by default`, () => {
@@ -49,7 +49,7 @@ describe(`Data reducer`, () => {
     const dispatch = jest.fn();
     const api = createAPI(dispatch);
     const apiMock = new MockAdapter(api);
-    const dataLoader = Operations.loadData();
+    const dataLoader = Operations.loadDataPromo();
 
     apiMock
       .onGet(`/films/promo`)
@@ -59,9 +59,53 @@ describe(`Data reducer`, () => {
       .then(() => {
         expect(dispatch).toHaveBeenCalled();
         expect(dispatch).toHaveBeenNthCalledWith(1, {
-          type: ActionType.LOAD_DATA,
+          type: ActionType.LOAD_DATA_PROMO,
           payload: [{fake: true}],
         });
       });
+  });
+
+  it(`Should make a correct API GET call for favorite data`, () => {
+    const dispatch = jest.fn();
+    const api = createAPI(dispatch);
+    const apiMock = new MockAdapter(api);
+    const dataLoader = Operations.loadDataFavorites();
+
+    apiMock
+      .onGet(`/favorite`)
+      .reply(200, {fake: true});
+
+    return dataLoader(dispatch, jest.fn(), api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_DATA_FAVORITES,
+          payload: {fake: true},
+        });
+      });
+  });
+
+  it(`Should make a correct call for loadDataItemReviews`, () => {
+    const action = ActionCreators.loadDataItemReviews(0);
+
+    expect(action.type).toEqual(`LOAD_DATA_REVIEWS`);
+  });
+
+  it(`Should make a correct call postReview`, () => {
+    const action = ActionCreators.postReview(0);
+
+    expect(action.type).toEqual(`POST_REVIEW`);
+  });
+
+  it(`Should make a correct call setToFavorites`, () => {
+    const action = ActionCreators.setToFavorites({fake: true});
+
+    expect(action.type).toEqual(`UPDATE_DATA_FAVORITES`);
+  });
+
+  it(`Should make a correct call changeDataItemCurrent`, () => {
+    const action = ActionCreators.changeDataItemCurrent(0);
+
+    expect(action.type).toEqual(`CHANGE_DATA_ACTIVE`);
   });
 });
